e33a1834dfe600f3757087a34f829c50c72b0412
Dilip Kr. Shukla <fusionstrings@gmail.com>
2021-04-12 15:36:48 +0200

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -10,7 +10,7 @@ async function handleRequest(request: Request) {
   if (pathname.startsWith("/style.css")) {
     //  Construct a new URL to style.css by using the URL
     //  of the script (mod.ts) as base (import.meta.url).
-    const style = new URL(style.css, import.meta.url);
+    const style = new URL("style.css", import.meta.url);
     // Fetch the asset and return the fetched response
     // to the client.
     const response = await fetch(style);
